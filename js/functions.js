cb09423500d5ae37bfc804d7eb64d0556d66490e
Manuel H <mhaeussermann@users.noreply.github.com>
2019-01-22 17:36:23 +0100

    Working on performace n stuff

--- js/functions.js
+++ js/functions.js
@@ -7,6 +7,8 @@ function drawText(text) {
   var ctx = c.getContext('2d');
   ctx.clearRect(0, 0, c.width, c.height);
   ctx.fillStyle = "#ffe303";
+  ctx.clearRect(20, 20, c.width, c.height);
+  ctx.fillStyle = "#fffff";
   ctx.fillRect(0, 0, c.width, c.height);
   ctx.fillStyle = "rgb(0, 0, 0)";
 
@@ -34,7 +36,7 @@ function drawText(text) {
   for (var j = 0; j <= 2; j++) {
     for (var i = 0; i <= 1; i++) {
       function at(row, column) {
-        return i == row && j == column;
+        return j == column;
       } 
     
       function adjust(number, amount, condition) {
@@ -46,44 +48,18 @@ function drawText(text) {
     
       var offsetX = 84.0;
       var offsetY = 88.0;
-    
-      function goLeft(amount, condition) {
-        offsetX = adjust(offsetX, -amount, condition);
-      }
-    
-      function goRight(amount, condition) {
-        offsetX = adjust(offsetX, amount, condition);
-      }
-    
-      function goUp(amount, condition) {
-        offsetY = adjust(offsetY, -amount, condition);
-      }
-    
-      function goDown(amount, condition) {
-        offsetY = adjust(offsetY, amount, condition);
-      }
 
       var jOffset = 0;
       if (j == 1) {
         jOffset = 291;
-      } else if (j == 2) {
-        jOffset = 565;
-      }
-      ctx.fillText(text.toUpperCase(), offsetX + jOffset, offsetY + (i * 31));
+      } 
+      ctx.fillText(text, offsetX + jOffset, offsetY);
     }
   }
 }
 
-function readURL(file) {
-  var imagePreview = ge('imagePreview');
-  imagePreview.src = URL.createObjectURL(file);
-  
-  setTimeout(updateText, 100);
-}
-
 function updateText() {
   var textField = ge('textField');
-  textField.value = textField.value.toUpperCase();
   drawText(textField.value);
 }
 
