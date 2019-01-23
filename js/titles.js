5b433a669d0fd90e34e01f0c2ee5f1d4692b0544
Manuel H <mhaeussermann@users.noreply.github.com>
2019-01-23 11:06:07 +0100

    Started implemented random title button

---
+++ js/titles.js
@@ -0,0 +1,15 @@
+var titles = [
+    'Test1',
+    'Test2',
+    'Test3',
+    'Test4',
+    'Test5'
+];
+
+
+function newTitle() {
+    var randomNumber = Math.floor(Math.random() * (titles.length)); 
+    var text = $('#textField');
+    text.val(titles[randomNumber]);
+    updateText();
+}
